b62e1233174ae310dd7fb26f4b130b751264fe4b
lucasperovani <lucasperovani@gmail.com>
2020-06-01 19:38:42 -0300

    Added article list. Visual fixed.

--- scripts/page.js
+++ scripts/page.js
@@ -1,6 +1,6 @@
 window.onscroll = function() {stickyMenuBar()};
 
-
+/* Sticky Menu Bar */
 var topMenuBar 		= document.getElementById("topMenuBar");
 var topMenuLogo 	= document.getElementById("topMenuBarLogo")
 
@@ -35,4 +35,60 @@ function stickyMenuBar() {
 	
 	}
 
+}
+
+
+/* XML Request */
+/* Como o site no github pages é estático e não devo desenvolver algo para que algum */
+/* usuário crie artigos no site, não irei me preocupar com XSS nesse JSON por enquanto.*/
+let xmlRequest = new XMLHttpRequest();
+let jsonFile;
+
+xmlRequest.onreadystatechange = function() 
+{
+	if (this.readyState == 4 && this.status == 200)
+	{
+		/* Parse Articles JSON */
+		jsonFile = JSON.parse(this.responseText);
+	  
+		/* Fill with articles */
+		body = document.getElementById("bodyContainer");
+		articles = jsonFile.articles.ids.split(';');
+
+		let iterator;
+		for (iterator = 0; iterator < articles.length; iterator++)
+		{
+			body.insertAdjacentHTML('beforeend',`
+			<div id="article-${articles[iterator]}" class="articleContainer" onclick="toggleArticle(${articles[iterator]})">
+
+				<h2 class="articleTitle">${jsonFile[articles[iterator]].Title}</h2>
+				<p class="articleDescription">${jsonFile[articles[iterator]].Description}</p>
+
+				<div id="article-${articles[iterator]}-Content" class="articleContent articleContentInvisibility">${jsonFile[articles[iterator]].Content}</div>
+
+			</div>
+			`);
+		}
+	}
+};
+  
+xmlRequest.open("GET", "../articles/articles.json", true);
+xmlRequest.send();
+
+
+/* Toogle Articles */
+function toggleArticle(articleID)
+{
+	let article = document.getElementById("article-" + articleID);
+
+	if (article.classList.contains("articleContainerOpened"))
+	{
+		article.classList.remove("articleContainerOpened");
+		document.getElementById("article-" + articleID + "-Content").classList.add("articleContentInvisibility");
+
+	} else {
+
+		article.classList.add("articleContainerOpened");
+		document.getElementById("article-" + articleID + "-Content").classList.remove("articleContentInvisibility");
+	}
 }
